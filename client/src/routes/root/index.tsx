b62c72b8cc142892e906a0524114d454c7b40537
enndy <91780685+enndylove@users.noreply.github.com>
2025-08-11 18:18:08 +0300

    clearning code | remove unusable files

--- client/src/routes/root/index.tsx
+++ client/src/routes/root/index.tsx
@@ -1,9 +1,6 @@
-import { useAuth } from "@/shared/hooks/useAuth";
-// import { Header } from "@/shared/components/Header";
 import { QueryClient } from "@tanstack/react-query";
 import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
-import { ThemeProvider } from "@/shared/theme/theme-provider";
 
 export const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,7 +12,6 @@ export const queryClient = new QueryClient({
 
 type MyRouterContext = {
   queryClient: typeof queryClient;
-  auth: ReturnType<typeof useAuth>;
 };
 
 const rootRoute = createRootRouteWithContext<MyRouterContext>()({
@@ -24,18 +20,15 @@ const rootRoute = createRootRouteWithContext<MyRouterContext>()({
 
 function RootComponent() {
   return (
-    <ThemeProvider>
-      <div className="min-h-svh flex flex-col">
-        {/* <Header /> */}
-        <main className="flex-1 bg-background">
-          {/* This is where child routes will render */}
-          <Outlet />
-        </main>
-        {import.meta.env.MODE === "development" && (
-          <TanStackRouterDevtools position="bottom-left" />
-        )}
-      </div>
-    </ThemeProvider>
+    <div className="min-h-svh flex flex-col">
+      <main className="flex-1 bg-background">
+        {/* This is where child routes will render */}
+        <Outlet />
+      </main>
+      {import.meta.env.MODE === "development" && (
+        <TanStackRouterDevtools position="bottom-left" />
+      )}
+    </div>
   );
 }
 
--- client/src/shared/entities/user.entity.ts
+++
@@ -1,5 +0,0 @@
-export type User = {
-  id: string; // uuid
-  email: string; // email
-  password?: string; // hashing password(optional in client)
-};
